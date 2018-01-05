edac30ca449977d2fa5ea893068ffe29f6cd5f5f
yevhen <eugenpechurin@gmail.com>
2018-01-05 15:52:06 +0000

    add geek_spider

---
+++ crawler/crawler/spiders/geek_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapy.spider import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+
+
+class GeekSpider(CrawlSpider):
+    name = 'geek_spider'
+    
+    start_url = ['https://geekbrains.ru/courses']
+    allowed_domains = ['geekbrains.ru']
+    
+    rules = (
+        Rule(
+            LinkExtractor(
+                restrict_xpaths=['//div[@class="gb-profession-cards-grid__content js-gb-profession-cards-grid-content"]'
+                                '//div[@class="gb-profession-cards-grid__item js-grid-item"]']
+                allow=r'geekbrains.ru/\w+/\w+$'
+            ),
+            callback='parse_item'
+        ),
+    )
+    
+    def parse_item(self, response):
+        selector = Selector(response)
